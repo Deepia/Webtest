2ebdc96ba45bdccead49165c860965fcc9cf5cf6
Deepia <kumardeepak.parsa@gmail.com>
2021-12-15 00:08:04 +0530

    dbcontext

---
+++ Models/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webtest.Models
+{
+    public class Subject
+    {
+        [Key]
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+        public string SubjectTerm { get; set; }
+        public string SubjectCredets { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Webtest.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<MVCContext>(options =>
+{
+    options.UseSqlServer("");
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
